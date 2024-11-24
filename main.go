5e784c565512b811376b74fd2cfb97d7a9b10bcc
Rem M. <morrig407@gmail.com>
2024-11-24 16:35:30 +0700

    Ask name, welcome the user

--- main.go
+++ main.go
@@ -6,4 +6,11 @@ import (
 
 func main() {
 	fmt.Println("Welcome to the Brain Games!")
+
+	var name string
+	for name == "" {
+		fmt.Printf("May I have your name? ")
+		fmt.Scanln(&name)
+	}
+	fmt.Printf("Hello, %s!\n", name)
 }
